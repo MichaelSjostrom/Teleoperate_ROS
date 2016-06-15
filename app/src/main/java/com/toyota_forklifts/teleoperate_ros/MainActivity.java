b814f1e2da8f33e0c0d03d8144492e73f9a7238d
Michael <micke_moj@hotmail.com>
2016-06-15 14:26:45 +0200

    tweaked the code

--- app/src/main/java/com/toyota_forklifts/teleoperate_ros/MainActivity.java
+++ app/src/main/java/com/toyota_forklifts/teleoperate_ros/MainActivity.java
@@ -1,7 +1,7 @@
 package com.toyota_forklifts.teleoperate_ros;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,9 +12,10 @@ import org.ros.android.view.VirtualJoystickView;
 import org.ros.node.NodeMainExecutor;
 import org.ros.namespace.NameResolver;
 import org.ros.node.NodeConfiguration;
-import org.ros.address.InetAddressFactory;
 import org.ros.android.BitmapFromCompressedImage;
 import com.github.rosjava.android_remocons.common_tools.apps.RosAppActivity;
+
+import java.io.IOException;
 
 import sensor_msgs.CompressedImage;
 
@@ -23,39 +24,26 @@ public class MainActivity extends RosAppActivity {
     private RosImageView<CompressedImage> cameraView;
     private VirtualJoystickView virtualJoystickView;
     private Button backButton;
-    private static final String cameraTopic = "camera/rgb/image_color/compressed_throttle";
 
     public MainActivity() {
         // The RosActivity constructor configures the notification title and
-        // ticker
-        // messages.
+        // ticker messages.
         super("android teleop", "android teleop");
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        //setContentView(R.layout.activity_main);
-        String defaultRobotName = getString(R.string.robot_name);
-        String defaultAppName = getString(R.string.app_name);
-
-        //setDefaultRobotName(defaultRobotName);
-
-        setDefaultAppName(defaultAppName);
-        setDefaultMasterName(defaultRobotName);
 
         setDashboardResource(R.id.top_bar);
         setMainWindowResource(R.layout.activity_main);
-
         super.onCreate(savedInstanceState);
 
         cameraView = (RosImageView<sensor_msgs.CompressedImage>) findViewById(R.id.image);
         cameraView.setMessageType(sensor_msgs.CompressedImage._TYPE);
         cameraView.setMessageToBitmapCallable(new BitmapFromCompressedImage());
         virtualJoystickView = (VirtualJoystickView) findViewById(R.id.virtual_joystick);
-        virtualJoystickView.setTopicName("/cmd_vel");
         backButton = (Button) findViewById(R.id.back_button);
-
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,21 +58,34 @@ public class MainActivity extends RosAppActivity {
 
         super.init(nodeMainExecutor);
 
-        NodeConfiguration nodeConfiguration = NodeConfiguration.newPublic(
-                InetAddressFactory.newNonLoopback().getHostAddress(),
-                getMasterUri());
+        try {
 
-        //Was 'getAppNameSpace(), not sure if getMasternameSpace() is correct.
-        NameResolver appNameSpace = getMasterNameSpace();
-        cameraView.setTopicName(appNameSpace.resolve(cameraTopic).toString());
+            java.net.Socket socket = new java.net.Socket(getMasterUri().getHost(), getMasterUri().getPort());
+            java.net.InetAddress local_network_address = socket.getLocalAddress();
+            socket.close();
+            
+            NodeConfiguration nodeConfiguration =
+                    NodeConfiguration.newPublic(local_network_address.getHostAddress(), getMasterUri());
 
-        nodeMainExecutor.execute(cameraView, nodeConfiguration
-                .setNodeName("android/camera_view"));
-        nodeMainExecutor.execute(virtualJoystickView,
-                nodeConfiguration.setNodeName("android/virtual_joystick"));
+            String joyTopic = remaps.get(getString(R.string.joystick_topic));
+            String camTopic = remaps.get(getString(R.string.camera_topic));
 
-        /*nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory
-                .newNonLoopback().getHostAddress(), getMasterUri());*/
+            NameResolver appNameSpace = getMasterNameSpace();
+            joyTopic = appNameSpace.resolve(joyTopic).toString();
+            camTopic = appNameSpace.resolve(camTopic).toString();
+
+            cameraView.setTopicName(camTopic);
+            virtualJoystickView.setTopicName(joyTopic);
+
+            nodeMainExecutor.execute(cameraView, nodeConfiguration
+                    .setNodeName("android/camera_view"));
+            nodeMainExecutor.execute(virtualJoystickView,
+                    nodeConfiguration.setNodeName("android/virtual_joystick"));
+
+        }catch(IOException e){
+            //Socket error
+            Log.e("TAG", e.toString());
+        }
 
     }
 
