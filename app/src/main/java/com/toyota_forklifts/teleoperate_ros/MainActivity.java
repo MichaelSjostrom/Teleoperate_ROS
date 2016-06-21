4f20e1814bd93bfcea40bc2a09f056798c2f059d
MichaelSjostrom <micke_moj@hotmail.com>
2016-06-21 13:20:54 +0200

    testing

--- app/src/main/java/com/toyota_forklifts/teleoperate_ros/MainActivity.java
+++ app/src/main/java/com/toyota_forklifts/teleoperate_ros/MainActivity.java
@@ -10,27 +10,44 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.github.rosjava.android_remocons.common_tools.apps.RosAppActivity;
+import com.google.common.collect.Lists;
+
+import org.ros.address.InetAddressFactory;
 import org.ros.android.view.RosImageView;
 import org.ros.android.view.VirtualJoystickView;
+import org.ros.android.view.visualization.layer.Layer;
 import org.ros.node.NodeMainExecutor;
 import org.ros.namespace.NameResolver;
 import org.ros.node.NodeConfiguration;
 import org.ros.android.BitmapFromCompressedImage;
-import com.github.rosjava.android_remocons.common_tools.apps.RosAppActivity;
+import org.ros.android.view.visualization.VisualizationView;
+import org.ros.android.view.visualization.layer.CameraControlListener;
+import org.ros.android.view.visualization.layer.OccupancyGridLayer;
+import org.ros.android.view.visualization.layer.LaserScanLayer;
+import org.ros.android.view.visualization.layer.RobotLayer;
+import org.ros.time.NtpTimeProvider;
+
 
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import sensor_msgs.CompressedImage;
 
 public class MainActivity extends RosAppActivity implements AdapterView.OnItemSelectedListener{
+
+    private static final String ROBOT_FRAME = "base_link";
+
 
     private RosImageView<CompressedImage> cameraView;
     private VirtualJoystickView virtualJoystickView;
     private Button backButton;
     private Spinner spinner = null;
     private ArrayList<String> spinnerArray = null;
+    private VisualizationView mapView;
 
     public MainActivity() {
         // The RosActivity constructor configures the notification title and
@@ -48,10 +65,11 @@ public class MainActivity extends RosAppActivity implements AdapterView.OnItemSe
         super.onCreate(savedInstanceState);
 
         //The view which the robot camera feed is sent to
-        cameraView = (RosImageView<sensor_msgs.CompressedImage>) findViewById(R.id.image);
+        cameraView = (RosImageView<sensor_msgs.CompressedImage>) findViewById(R.id.camera_view);
         //Setting the images to be compressed
         cameraView.setMessageType(sensor_msgs.CompressedImage._TYPE);
         cameraView.setMessageToBitmapCallable(new BitmapFromCompressedImage());
+        mapView = (VisualizationView) findViewById(R.id.map_view);
         //The joystick which is used to navigate the robot remotely
         virtualJoystickView = (VirtualJoystickView) findViewById(R.id.virtual_joystick);
         //Back button in top left corner to get back to the view where connection to robot is done
@@ -83,7 +101,9 @@ public class MainActivity extends RosAppActivity implements AdapterView.OnItemSe
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
+        //mapView.getCamera().jumpToFrame(ROBOT_FRAME);
 
+        mapView.onCreate(Lists.<Layer>newArrayList(new OccupancyGridLayer("map")));
     }
 
     @Override
@@ -104,21 +124,59 @@ public class MainActivity extends RosAppActivity implements AdapterView.OnItemSe
             //Creates string variables with the respective name from string resource file
             String joyTopic = remaps.get(getString(R.string.joystick_topic));
             String camTopic = remaps.get(getString(R.string.camera_topic));
+            String mapTopic = remaps.get(getString(R.string.map_topic));
+            String scanTopic = remaps.get(getString(R.string.scan_topic));
 
             //Resolves the namespace for each topic, e.g. /blabla/blabla/blabla
             NameResolver appNameSpace = getMasterNameSpace();
             joyTopic = appNameSpace.resolve(joyTopic).toString();
             camTopic = appNameSpace.resolve(camTopic).toString();
+            mapTopic = appNameSpace.resolve(mapTopic).toString();
+            scanTopic = appNameSpace.resolve(scanTopic).toString();
 
-            //Sets the topicname for each topic
+            //Sets the topic name for each topic
             virtualJoystickView.setTopicName(joyTopic);
             cameraView.setTopicName(camTopic);
 
             //Executes each no
             nodeMainExecutor.execute(cameraView, nodeConfiguration
-                    .setNodeName("android/camera_view"));
+                    .setNodeName(getString(R.string.camera_view_node)));
             nodeMainExecutor.execute(virtualJoystickView,
-                    nodeConfiguration.setNodeName("android/virtual_joystick"));
+                    nodeConfiguration.setNodeName(getString(R.string.virtual_joystick_node)));
+
+            ViewControlLayer viewControlLayer = new ViewControlLayer(this, nodeMainExecutor.getScheduledExecutorService(),
+                    cameraView, mapView);
+            OccupancyGridLayer occupancyGridLayer = new OccupancyGridLayer(mapTopic);
+
+            //Arraylist for all layers
+            List<Layer> layers = new ArrayList<>();
+
+            //Adding layers
+            //layers.add(viewControlLayer);
+            layers.add(new OccupancyGridLayer(mapTopic));
+            layers.add(new LaserScanLayer(scanTopic));
+            //layers.add(new RobotLayer(ROBOT_FRAME));
+
+            //Adds layer to mapView
+            //mapView.onCreate(layers);
+
+            /*mapView.onCreate(Lists.<Layer>newArrayList(
+                    viewControlLayer,
+                    new OccupancyGridLayer(mapTopic),
+                    new LaserScanLayer(scanTopic))
+            );*/
+
+            //mapView.onCreate(layers);
+
+
+            NtpTimeProvider ntpTimeProvider = new NtpTimeProvider(
+                    InetAddressFactory.newFromHostString("192.168.42.32"),
+                    nodeMainExecutor.getScheduledExecutorService()
+            );
+
+            ntpTimeProvider.startPeriodicUpdates(1, TimeUnit.MINUTES);
+            nodeConfiguration.setTimeProvider(ntpTimeProvider);
+            nodeMainExecutor.execute(mapView, nodeConfiguration.setNodeName(getString(R.string.map_view_node)));
 
         }catch(IOException e){
             //Socket error
---
+++ app/src/main/java/com/toyota_forklifts/teleoperate_ros/ViewControlLayer.java
@@ -0,0 +1,74 @@
+package com.toyota_forklifts.teleoperate_ros;
+
+import java.util.concurrent.ExecutorService;
+import android.content.Context;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.ros.android.view.RosImageView;
+import org.ros.android.view.visualization.Camera;
+import org.ros.android.view.visualization.RotateGestureDetector;
+import org.ros.android.view.visualization.VisualizationView;
+import org.ros.android.view.visualization.layer.CameraControlLayer;
+import org.ros.android.view.visualization.layer.CameraControlListener;
+import org.ros.concurrent.ListenerGroup;
+import org.ros.concurrent.SignalRunnable;
+import org.ros.node.ConnectedNode;
+import org.ros.rosjava_geometry.FrameTransformTree;
+
+import java.util.concurrent.ExecutorService;
+
+import sensor_msgs.CompressedImage;
+
+/**
+ * Created by mitch on 2016-06-21.
+ */
+public class ViewControlLayer extends CameraControlLayer {
+
+    private static final String ROBOT_FRAME = "base_link";
+    private final Context context;
+    private final ListenerGroup<CameraControlListener> listeners ;
+
+    private GestureDetector translateGestureDetector;
+    private RotateGestureDetector rotateGestureDetector;
+    private ScaleGestureDetector zoomGestureDetector;
+
+    private RosImageView<CompressedImage> cameraView;
+    private VisualizationView mapView;
+    private ViewGroup mainLayout;
+    private ViewGroup sideLayout;
+    private boolean mapViewGestureAvaiable;
+
+    private enum ViewMode {
+        CAMERA, MAP
+    };
+    private ViewMode viewMode;
+
+    public ViewControlLayer(Context context,
+                            ExecutorService executorService,
+                            RosImageView<sensor_msgs.CompressedImage> cameraView,
+                            VisualizationView mapView){
+
+        //super(context,executorService);
+
+        this.context = context;
+        listeners = new ListenerGroup<CameraControlListener>(executorService);
+
+        this.cameraView = cameraView;
+        this.mapView = mapView;
+        this. mainLayout = mainLayout;
+        this.sideLayout = sideLayout;
+
+        viewMode = ViewMode.CAMERA;
+
+
+        //this.mapView.setClickable(true);
+        //this.cameraView.setClickable(true);
+
+        //mapViewGestureAvaiable = false;
+    }
+}
